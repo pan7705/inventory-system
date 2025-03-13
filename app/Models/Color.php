ba70bf48b50fff0e121baeff223546bf7ea08cfb
Irfan Haqim <irfanhaqim4039@gmail.com>
2025-03-13 11:45:08 +0800

    crud color management

--- app/Models/Color.php
+++ app/Models/Color.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Color extends Model
 {
-    protected $table = 'color';
+    protected $table = 'colors';
 
     protected $fillable = [
         'name',
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">
+                    {{ __('Dashboard') }}
+                    <div class="card-toolbar">
+                        <a href="{{ route('color.index') }}" class="btn btn-primary">Create Color</a>
+                    </div>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
